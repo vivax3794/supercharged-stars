d4cd313bae60372a5ba6f7a9a310cb13957e34a6
vivax3794 <vivax3794@protonmail.com>
2022-07-25 06:20:26 +0200

    something

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,6 @@ import { relaunch } from '@tauri-apps/api/process'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-try {
-    const { shouldUpdate, manifest } = await checkUpdate()
-    if (shouldUpdate) {
-        // display dialog
-        await installUpdate()
-        // install complete, restart app
-        await relaunch()
-    }
-} catch (error) {
-    console.log(error)
-}
 
 
 createApp(App).mount('#app')
