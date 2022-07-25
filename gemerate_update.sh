ff47a9d9e36807eab4f31714e1daf1d1a51e5782
vivax3794 <vivax3794@protonmail.com>
2022-07-25 07:29:20 +0200

    make updates work

--- gemerate_update.sh
+++
@@ -1,2 +0,0 @@
-cp update_template.json update.json
-sed -i -e s/VERSION/${1}/g update.json
---
+++ generate_update.sh
@@ -0,0 +1,9 @@
+cp update_template.json update.json
+sed -i -e s/VERSION/${1}/g update.json
+darwin="$(curl https://github.com/vivax3794/supercharged-stars/releases/download/v${1}/supercharged-stars.app.tar.gz.sig -L)"
+linux="$(curl https://github.com/vivax3794/supercharged-stars/releases/download/v${1}/supercharged-stars_0.1.10_amd64.AppImage.tar.gz.sig -L)"
+windows="$(curl https://github.com/vivax3794/supercharged-stars/releases/download/v${1}/supercharged-stars_0.1.10_x64_en-US.msi.zip.sig -L)"
+
+sed -i -e "s/DARWIN_SIG/${darwin}/g" update.json
+sed -i -e "s/LINUX_SIG/${linux}/g" update.json
+sed -i -e "s/WINDOWS_SIG/${windows}/g" update.json
