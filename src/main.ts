3daff4fca1f396104ecafbf62d91b9a7cd49aca4
vivax3794 <vivax3794@protonmail.com>
2022-07-25 06:06:39 +0200

    fix stuff

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
+
+import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
+import { relaunch } from '@tauri-apps/api/process'
+
 import { createApp } from 'vue'
 import App from './App.vue'
+
+try {
+    const { shouldUpdate, manifest } = await checkUpdate()
+    if (shouldUpdate) {
+        // display dialog
+        await installUpdate()
+        // install complete, restart app
+        await relaunch()
+    }
+} catch (error) {
+    console.log(error)
+}
+
 
 createApp(App).mount('#app')
